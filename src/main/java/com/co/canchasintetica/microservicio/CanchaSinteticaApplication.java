93d44885115de60845894f2b96f6e60b73492bee
Cristian Camilo Arcila Delgado  - Ceiba Software <camilo.arcila@ceiba.com.co>
2021-03-15 22:45:20 -0500

    ajustes

--- src/main/java/com/co/canchasintetica/microservicio/CanchaSinteticaApplication.java
+++ src/main/java/com/co/canchasintetica/microservicio/CanchaSinteticaApplication.java
@@ -2,7 +2,9 @@ package com.co.canchasintetica.microservicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.co.canchasintetica"})
 @SpringBootApplication
 public class CanchaSinteticaApplication {
 	public static void main(String[] args) {
